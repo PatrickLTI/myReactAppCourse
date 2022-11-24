450cd319d4aa91ff0054be4bbbc6cc4c3f300079
Pat <la_spirou@hotmail.com>
2022-11-24 09:40:58 -0500

    bringing over earlier code to react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [color, setColor] = React.useState("blue");
+  const [count, setCount] = React.useState(0);
+
+  console.log(color);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+
+      {/* This component has two children that display a message and a clock */}
+      <DisplayMessage color={color} />
+      <Clock2 time={new Date().toLocaleTimeString()} />
+      <p>Total count: {count} </p>
+      <button onClick={() => { setColor(toggle(color)); setCount(count + 1) }}>
+        Click me React To Change Colors
+      </button>
+
     </div>
+
   );
 }
+function DisplayMessage(props) {
+  return <h1 style={{ color: props.color }}>Hello React World NEW Format</h1>;
+
+}
+// function Clock(props) {
+//     return <p>React Clock: {props.time} </p>
+// }
+
+function toggle(color) {
+  if (color === "blue") {
+    return "red";
+  } else {
+    return "blue";
+  }
+}
+
+// class Clock2 extends React.Component {
+//     constructor(props) {
+//         super(props);
+//     }
+//     render() {
+//         return (<p>React Clock2: {this.props.time}</p>);
+//     }
+// }
+
+function Clock2(props) {
+  return (<p> React Clock2: {props.time}</p>);
+}
+
+
+// setInterval(() => root.render(<Main />), 1000);
+
+
+// root.render(<Main />) // first load
+
+
+
 
 export default App;
