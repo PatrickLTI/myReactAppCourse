90b998607a2566173f5f7a47a7d9ee6e3eb92133
Pat <la_spirou@hotmail.com>
2022-11-25 10:20:35 -0500

    Some saves

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Main from './components/Main';
+// import Main from './components/Main';
 import {Route, Routes} from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
